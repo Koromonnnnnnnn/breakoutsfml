0acf1b3df4b137ec7560a2b9ef2381f941d3cc16
Koromonnnnnnnn <764363@dpsk12.net>
2024-10-18 10:21:19 -0600

    .

--- breakout/main.cpp
+++ breakout/main.cpp
@@ -3,118 +3,115 @@
 // Brick Class
 class Brick {
 private:
-	float width;
-	float height;
-	float xpos;
-	float ypos;
-	bool Dead;
-	sf::RectangleShape shape;
+    float width;
+    float height;
+    float xpos;
+    float ypos;
+    bool Dead;
+    sf::RectangleShape shape;
 public:
+    Brick(float x, float y, float w, float h) : xpos(x), ypos(y), width(w), height(h), Dead(false) {
+        shape.setSize(sf::Vector2f(width, height));
+        shape.setPosition(xpos, ypos);
+        shape.setFillColor(sf::Color::Red);
+    }
 
-	// Constructor initializes the brick
-	Brick(float x, float y, float w, float h) {
-		xpos = x;
-		ypos = y;
-		width = w;
-		height = h;
-		Dead = false;
+    void draw(sf::RenderWindow& window) {
+        if (!Dead) {
+            window.draw(shape);
+        }
+    }
 
-		// Set up the shape
-		shape.setSize(sf::Vector2f(width, height));
-		shape.setPosition(xpos, ypos);
-		shape.setFillColor(sf::Color::Red);
-	}
+    void killBrick() {
+        Dead = true;
+        shape.setFillColor(sf::Color::Transparent);
+    }
 
-	void draw(sf::RenderWindow& window) {
-		if (!Dead) {
-			window.draw(shape);
-		}
-	}
-
-	void killBrick() {
-		Dead = true;
-		shape.setFillColor(sf::Color::Transparent); // Make the brick invisible
-	}
+    bool isDead() const { return Dead; }
+    float getX() const { return xpos; }
+    float getY() const { return ypos; }
+    float getWidth() const { return width; }
+    float getHeight() const { return height; }
 };
 
 class Ball {
 private:
-	float radius;
-	float xpos;
-	float ypos;
-	float xVelocity;
-	float yVelocity;
-	sf::CircleShape shape;
+    float radius;
+    float xpos;
+    float ypos;
+    float xVelocity;
+    float yVelocity;
+    sf::CircleShape shape;
 public:
-	Ball(float x, float y, float xvel, float yvel) {
-		xpos = x;
-		ypos = y;
-		xVelocity = xvel;
-		yVelocity = yvel;
-		shape.setRadius(radius);
-		shape.setPosition(xpos, ypos);
-		shape.setFillColor(sf::Color::White);
-	}
-	
-	void draw(sf::RenderWindow& window) {
-		window.draw(shape);
-	}
+    Ball(float x, float y, float xvel, float yvel) : xpos(x), ypos(y), xVelocity(xvel), yVelocity(yvel), radius(10.f) {
+        shape.setRadius(radius);
+        shape.setPosition(xpos, ypos);
+        shape.setFillColor(sf::Color::White);
+    }
 
-	void move(float windowWidth, float windowHeight) {
-		xpos += xVelocity;
+    void draw(sf::RenderWindow& window) {
+        shape.setPosition(xpos, ypos);
+        window.draw(shape);
+    }
 
-		if (xpos <= 0 || xpos + radius * 2 >= windowWidth) {
-			xVelocity = -xVelocity; // Reverse horizontal direction
-		}
+    void move(float windowWidth, float windowHeight) {
+        xpos += xVelocity;
+        ypos += yVelocity;
 
+        if (xpos <= 0 || xpos + radius * 2 >= windowWidth) {
+            xVelocity = -xVelocity;
+        }
 
-	bool brickCollision(Brick & brick) {
-		if (!brick.isDead() &&
-			xpos + radius > brick.getX() &&
-			xpos - radius < brick.getX() + brick.getWidth() &&
-			ypos + radius > brick.getY() &&
-			ypos - radius < brick.getY() + brick.getHeight()) {
+        if (ypos <= 0 || ypos + radius * 2 >= windowHeight) {
+            yVelocity = -yVelocity;
+        }
+    }
 
-			brick.killBrick();
-			reflect();
-			return true;
-		}
-		return false;
-	}
+    bool brickCollision(Brick& brick) {
+        if (!brick.isDead() &&
+            xpos + radius > brick.getX() &&
+            xpos - radius < brick.getX() + brick.getWidth() &&
+            ypos + radius > brick.getY() &&
+            ypos - radius < brick.getY() + brick.getHeight()) {
 
+            yVelocity = -yVelocity;
+            brick.killBrick();
+            return true;
+        }
+        return false;
+    }
 };
 
 int main() {
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Breakout");
+    Brick brick1(100, 100, 50, 20);
+    Brick brick2(160, 100, 50, 20);
+    Brick brick3(220, 100, 50, 20);
 
-	sf::RenderWindow window(sf::VideoMode(800, 600), "Breakout");
-	Brick brick1(100, 100, 50, 20);
-	Brick brick2(160, 100, 50, 20);
-	Brick brick3(220, 100, 20, 20);
+    Ball ball(0, 0, 5, 5);
 
-	Ball ball(0, 0, 50, 20);
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
 
-	while (window.isOpen()) {
-		sf::Event event;
-		while (window.pollEvent(event)) {
-			if (event.type == sf::Event::Closed)
-				window.close();
-		}
+        window.clear();
 
-		window.clear();
+        ball.move(800, 600);
 
-		ball.move(800, 600);
+        brick1.draw(window);
+        brick2.draw(window);
+        brick3.draw(window);
 
-		brick1.draw(window);
-		brick2.draw(window);
-		brick3.draw(window);
+        ball.draw(window);
 
-		ball.draw(window);
+        ball.brickCollision(brick1);
+        ball.brickCollision(brick2);
+        ball.brickCollision(brick3);
 
-		ball.brickCollision(brick1);
-		ball.brickCollision(brick2);
-		ball.brickCollision(brick3);
-
-		window.display();
-	}
-	return 0;
+        window.display();
+    }
+    return 0;
 }
